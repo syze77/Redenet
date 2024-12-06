a9873f31d9a008a3cea70dcfd3723da4f7a64e5e
syze77 <167648171+syze77@users.noreply.github.com>
2024-12-06 15:41:06 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('contato-form');
-    
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        alert('Mensagem enviada com sucesso!');
-        form.reset();
-    });
-});
